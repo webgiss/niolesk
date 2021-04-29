3e08671ccecb6f2091e123b2c03073c122806202
gissehel <public-devgit-common@gissehel.org>
2021-04-29 21:45:32 +0200

    VIEW : Redesign position on small width display

--- src/views/Render/Render.js
+++ src/views/Render/Render.js
@@ -4,7 +4,7 @@ import './Render.css'
 
 const Render = ({ diagramUrl, diagramEditUrl }) => {
     return <div className='Render'>
-        <img alt='Diagram' src={diagramUrl} />
+        <img alt='Diagram' className='RenderImage' src={diagramUrl} />
         <p><a href={diagramEditUrl}>Edit this diagram.</a></p>
     </div>
 }
