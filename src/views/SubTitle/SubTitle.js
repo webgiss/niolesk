54ce4ee86b0cc43819fde486758b8c7be4ddee87
gissehel <public-devgit-common@gissehel.org>
2021-04-30 18:27:12 +0200

    FEATURE : Add git hash to the version

--- src/views/SubTitle/SubTitle.js
+++ src/views/SubTitle/SubTitle.js
@@ -1,11 +1,16 @@
+import GitInfo from 'react-git-info/macro';
 import React from 'react';
 
 import './SubTitle.css'
 
+const gitInfo = GitInfo();
+const version = `${process.env.REACT_APP_VERSION}-${gitInfo.commit.hash.substr(0,8)}`;
+
+
 const SubTitle = () => {
     return <div className='SubTitle'>
         <div>Edit <b>diagrams</b> from <b>textual</b> descriptions! : A <a href='https://kroki.io'>kroki</a> interface.</div>
-        <div className='SubTitleSmall'>Github project page: <a href='https://github.com/webgiss/niolesk/'>https://github.com/webgiss/niolesk/</a> - Version { process.env.REACT_APP_VERSION }</div>
+        <div className='SubTitleSmall'>Github project page: <a href='https://github.com/webgiss/niolesk/'>https://github.com/webgiss/niolesk/</a> - Version { version }</div>
     </div>
 }
 
