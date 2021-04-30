4a0e9e8d64e31cf56020f2404c7e125d97606b3e
gissehel <public-devgit-common@gissehel.org>
2021-04-30 15:16:38 +0200

    VIEW : Add version in subtitle

--- src/views/SubTitle/SubTitle.js
+++ src/views/SubTitle/SubTitle.js
@@ -5,7 +5,7 @@ import './SubTitle.css'
 const SubTitle = () => {
     return <div className='SubTitle'>
         <div>Edit <b>diagrams</b> from <b>textual</b> descriptions! : A <a href='https://kroki.io'>kroki</a> interface.</div>
-        <div className='SubTitleSmall'>Github project page: <a href='https://github.com/webgiss/niolesk/'>https://github.com/webgiss/niolesk/</a></div>
+        <div className='SubTitleSmall'>Github project page: <a href='https://github.com/webgiss/niolesk/'>https://github.com/webgiss/niolesk/</a> - Version { process.env.REACT_APP_VERSION }</div>
     </div>
 }
 
