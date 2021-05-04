e51016fe6e6a6b42bfd7952313cd179568cc1450
gissehel <public-devgit-common@gissehel.org>
2021-05-04 23:35:19 +0200

    BUGFIX : The "position" attribute of Monaco editor is not reset each time the text changes

--- src/views/Editor/Editor.js
+++ src/views/Editor/Editor.js
@@ -2,20 +2,50 @@ import React from 'react';
 import MonacoEditor from '@uiw/react-monacoeditor';
 import './Editor.css'
 
-const Editor = ({ text, onTextChanged }) => {
-    return <div className='Editor'>
-        <MonacoEditor 
-            language="plaintext"
-            onChange={onTextChanged}
-            value={text}
-            
-            options={{
-                theme: 'vs',
-                automaticLayout: false,
-            }}
-            height='700px'
-        />
-    </div>
+class Editor extends React.Component {
+    get shouldUpdate() {
+        return this._shouldUpdate === undefined ? true : this._shouldUpdate
+    }
+
+    set shouldUpdate(value) {
+        this._shouldUpdate = value;
+    }
+
+    shouldComponentUpdate(nextProps) {
+        if (this._editor) {
+            const editorText = this._editor.editor.getValue();
+            const nextPropsText = nextProps.text;
+
+            if (nextPropsText === editorText) {
+                this.shouldUpdate = false;
+            } else {
+                this.shouldUpdate = true;
+            }
+            return this.shouldUpdate;
+        } else {
+            return true;
+        }
+    }
+
+    render() {
+        const { text, onTextChanged } = this.props;
+        const { shouldUpdate } = this;
+
+        return <div className='Editor'>
+            <MonacoEditor
+                className='MonacoEditor'
+                ref={(ref) => this._editor = ref}
+                language="plaintext"
+                onChange={onTextChanged}
+                value={shouldUpdate ? text : null}
+                options={{
+                    theme: 'vs',
+                    automaticLayout: false,
+                }}
+                height='700px'
+            />
+        </div>
+    }
 }
 
 export default Editor;
