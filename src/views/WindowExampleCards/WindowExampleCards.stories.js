7e955dc86aa4c5e86a84ee6ca3463d3b21921628
gissehel <public-devgit-common@gissehel.org>
2021-09-16 18:40:09 +0200

    STORYBOOK : Fix WindowExample* storybooks

--- src/views/WindowExampleCards/WindowExampleCards.stories.js
+++ src/views/WindowExampleCards/WindowExampleCards.stories.js
@@ -1,15 +1,36 @@
 import React from 'react';
-import WindowExampleCards from './WindowExampleCards';
+import WindowExampleCards from './index';
+import { getReduxMockDecorator } from '../storybook/store';
 import { getComponenent } from '../storybook/stories';
+import exampleData from "../../examples/data";
 
 export default {
     title: 'Components/WindowExampleCards',
     component: WindowExampleCards,
 };
 
-const Template = (args) => <WindowExampleCards {...args} />;
+const defaultState = {
+    editor: { 
+        renderUrl : 'https://kroki.io/'
+    },
+    example: { 
+        windowExampleCardsOpened: true,
+        windowExampleDetailsOpened: false,
+        exampleIndex: 2,
+        examples: exampleData,
+    }
+};
+
+const Template = (args) => {
+    const { setState, decorator } = getReduxMockDecorator()
+    let state = defaultState
+
+    setState(state)
+    return decorator(() => <WindowExampleCards {...args} />)
+};
 
 const defaultArgs = {
+    open: true,
 };
 
 export const Default = getComponenent(Template, { ...defaultArgs });
--- src/views/WindowExampleDetail/WindowExampleDetail.stories.js
+++ src/views/WindowExampleDetail/WindowExampleDetail.stories.js
@@ -1,13 +1,33 @@
 import React from 'react';
-import WindowExampleDetail from './WindowExampleDetail';
+import WindowExampleDetail from './index';
 import { getComponenent } from '../storybook/stories';
+import { getReduxMockDecorator } from '../storybook/store';
+import exampleData from "../../examples/data";
 
 export default {
     title: 'Components/WindowExampleDetail',
     component: WindowExampleDetail,
 };
 
-const Template = (args) => <WindowExampleDetail {...args} />;
+const defaultState = {
+    editor: { 
+        renderUrl : 'https://kroki.io/'
+    },
+    example: { 
+        windowExampleCardsOpened: false,
+        windowExampleDetailsOpened: true,
+        exampleIndex: 2,
+        examples: exampleData,
+    }
+};
+
+const Template = (args) => {
+    const { setState, decorator } = getReduxMockDecorator()
+    let state = defaultState
+
+    setState(state)
+    return decorator(() => <WindowExampleDetail {...args} />)
+};
 
 const defaultArgs = {
 };
