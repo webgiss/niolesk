41f8991cf05f80875ddc3defb09b3ebee20c4017
gissehel <public-devgit-common@gissehel.org>
2021-12-29 11:22:23 +0100

    BUGFIX : Fix react warning about 'basic' and 'basic="true"'

--- src/views/SubTitle/SubTitle.js
+++ src/views/SubTitle/SubTitle.js
@@ -8,7 +8,7 @@ const gitInfo = GitInfo();
 const version = `${process.env.REACT_APP_VERSION}-${gitInfo.commit.hash.substr(0, 8)}`;
 
 const SubTitle = () => {
-    return <div className='SubTitle' basic>
+    return <div className='SubTitle' basic="true">
         <div>Edit <b>diagrams</b> from <b>textual</b> descriptions! : A <a href='https://kroki.io'>kroki</a> interface.</div>
         <div className='SubTitleSmall'>Github project page: <a href='https://github.com/webgiss/niolesk/'>https://github.com/webgiss/niolesk/</a> - Version {version}</div>
     </div>
